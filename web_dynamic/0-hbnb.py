039ac0a09501991897440888fed5999f94baba8b
timmoh-king <timothykariuki43@gmail.com>
2023-08-05 20:20:04 +0300

    0-hbnb.py

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -6,8 +6,10 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
-from flask import Flask, render_template
-app = Flask(__name__, template_folder='templates')
+from flask import Flask, render_template, url_for
+import uuid
+
+app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
@@ -18,7 +20,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/hbnb/', strict_slashes=False)
+@app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -34,7 +36,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,

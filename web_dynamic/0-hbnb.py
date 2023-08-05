2f28d092f368086871b9664f82bf2d45dbd57797
timmoh-king <timothykariuki43@gmail.com>
2023-08-05 19:59:31 +0300

    web_dynamic task 1

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -7,7 +7,7 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
